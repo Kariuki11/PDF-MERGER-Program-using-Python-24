39e38a903f1b65749d107021eed5160be0355b9b
Kariuki11 <irungukenneth433@gmail.com>
2024-07-02 17:23:45 +0300

    New changes

--- pdfMerger.py
+++ pdfMerger.py
@@ -4,33 +4,33 @@ import os
 
 merger = PyPDF2.PdfFileMerger()
 
-pdf_folder = 'C:/Users/Administrator/Desktop/PDF MERGER/pdf folders'
+pdf_folder = r'C:\Users\Administrator\Desktop\PDF MERGER\pdf folders'
 
 for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
         merger.append(file)
     merger.write("combinedBSUniDocs.pdf")
 
-import PyPDF2
-import os
+# import PyPDF2
+# import os
 
-# Create a PdfFileMerger object
-merger = PyPDF2.PdfFileMerger()
+# # Create a PdfFileMerger object
+# merger = PyPDF2.PdfFileMerger()
 
-# Specify the folder containing the PDF files
-pdf_folder = 'C:/Users/Administrator/Desktop/PDF MERGER/pdf folders'
+# # Specify the folder containing the PDF files
+# pdf_folder = 'C:/Users/Administrator/Desktop/PDF MERGER/pdf folders'
 
-# List of specific PDF files to merge
-pdf_files = [
-    "PCEA UMBUI YOUTH FELLOWSHIP.pdf",
-    "Gloxad Academy road map.pdf"
-]
+# # List of specific PDF files to merge
+# pdf_files = [
+#     "PCEA UMBUI YOUTH FELLOWSHIP.pdf",
+#     "Gloxad Academy road map.pdf"
+# ]
 
-# Loop through the list of PDF files and append each to the merger
-for pdf_file in pdf_files:
-    file_path = os.path.join(pdf_folder, pdf_file)
-    merger.append(file_path)
+# # Loop through the list of PDF files and append each to the merger
+# for pdf_file in pdf_files:
+#     file_path = os.path.join(pdf_folder, pdf_file)
+#     merger.append(file_path)
 
-# Write the merged PDF to a new file
-merger.write("combinedBSUniDocs.pdf")
-merger.close()
+# # Write the merged PDF to a new file
+# merger.write("combinedBSUniDocs.pdf")
+# merger.close()
