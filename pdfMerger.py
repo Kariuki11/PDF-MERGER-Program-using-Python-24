29e0831f450b56a9d93284e1f25ad53d6aacc6cd
Kariuki11 <irungukenneth433@gmail.com>
2024-07-02 17:25:47 +0300

    changes

--- pdfMerger.py
+++ pdfMerger.py
@@ -34,3 +34,28 @@
 # # Write the merged PDF to a new file
 # merger.write("combinedBSUniDocs.pdf")
 # merger.close()
+
+
+import PyPDF2
+import os
+
+# Create a PdfMerger object
+merger = PyPDF2.PdfMerger()
+
+# Specify the folder containing the PDF files
+pdf_folder = r'C:\Users\Administrator\Desktop\PDF MERGER\pdf folders'
+
+# List of specific PDF files to merge
+pdf_files = [
+    "PCEA UMBUI YOUTH FELLOWSHIP.pdf",
+    "Gloxad Academy road map.pdf"
+]
+
+# Loop through the list of PDF files and append each to the merger
+for pdf_file in pdf_files:
+    file_path = os.path.join(pdf_folder, pdf_file)
+    merger.append(file_path)
+
+# Write the merged PDF to a new file
+merger.write("combinedBSUniDocs.pdf")
+merger.close()
