5be975faf3dc0e26b931b9a1223513fb0fe356d6
Kariuki11 <irungukenneth433@gmail.com>
2024-07-02 17:06:42 +0300

    New commits

--- pdfMerger.py
+++ pdfMerger.py
@@ -3,5 +3,7 @@ import sys
 import os
 
 for file in os.listdir(os.curdir):
-    if file.endswith('.pdf'):
-        print(file)
+    if file.endswith(".pdf"):
+        merger = PyPDF2.PdfFileMerger()
+        merger.append(file)
+    merger.write("combinedBSUniDocs.pdf")
